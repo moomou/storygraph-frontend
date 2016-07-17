8374762249a0d6d7b94e6d050d10fa741e84be68
Aysar Khalid <ak@akpro.net>
2016-07-17 02:10:18 -0700

    Added graph

--- js/graph.js
+++ js/graph.js
@@ -1,3 +1,23 @@
+var POS_TO_NAME = {
+  "ADJ": {"id":1, "name":"adjective"},
+  "ADP": {"id":2, "name":"adposition"},
+  "ADV": {"id":3, "name":"adverb"},
+  "AUX": {"id":4, "name":"auxiliary verb"},
+  "CONJ":  {"id":5, "name":"coordinating conjunction"},
+  "DET":   {"id":6, "name":"determiner"},
+  "INTJ":  {"id":7, "name":"interjection"},
+  "NOUN":  {"id":8, "name":"noun"},
+  "NUM":   {"id":9, "name":"numeral"},
+  "PART":  {"id":10, "name":"particle"},
+  "PRON":  {"id":11, "name":"pronoun"},
+  "PROPN": {"id":12, "name":"proper noun"},
+  "PUNCT": {"id":13, "name":"punctuation"},
+  "SCONJ": {"id":14, "name":"subordinating conjunction"},
+  "SYM":   {"id":15, "name":"symbol"},
+  "VERB":  {"id":16, "name":"verb"},
+  "X":     {"id":17, "name":"other"}
+};
+
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
@@ -5,42 +25,86 @@ var svg = d3.select("svg"),
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody())
+    .force("link", d3.forceLink().id(function(d) { return d.form; }).distance(30))
+    .force("charge", d3.forceManyBody(-50))
+    .force("collide", d3.forceCollide(40).iterations(1))
     .force("center", d3.forceCenter(width / 2, height / 2));
 
-d3.json("miserables.json", function(error, graph) {
+d3.json("../tests/data/demo.json", function(error, graph) {
   if (error) throw error;
 
+  var verticies = [];
+  for (var i in graph.nodes) {
+    for (var j in graph.nodes) {
+      if (graph.nodes[j].id == graph.nodes[i].head) {
+        verticies.push({"source":graph.nodes[i].form, "target":graph.nodes[j].form});
+        break;
+      }
+    }
+  }
+
+  // build the arrow.
+svg.append("svg:defs").selectAll("marker")
+  .data(["end"])      // Different link/path types can be defined here
+  .enter().append("svg:marker")    // This section adds in the arrows
+    .attr("id", String)
+    .style("fill", "#999")
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 19)
+    .attr("refY", 0)
+    .attr("markerWidth", 3)
+    .attr("markerHeight", 3)
+    .attr("orient", "auto")
+  .append("svg:path")
+    .attr("d", "M0,-5L10,0L0,5");
+
   var link = svg.append("g")
-      .attr("class", "links")
+    .attr("class", "links")
     .selectAll("line")
-    .data(graph.links)
+    .data(verticies)
     .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+    .attr("marker-end", "url(#end)")
+    .attr("stroke-width", function(d) { return 3; });
 
-
-  var node = svg.append("g")
-      .attr("class", "nodes")
-    .selectAll("circle")
+  var node = svg.selectAll("g.node")
     .data(graph.nodes)
-    .enter().append("circle")
-      .attr("r", 5)
-      .attr("fill", function(d) { return color(d.group); })
-      .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
+    .enter().append("svg:g")
+    .attr("class", "node")
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended));
+    
+  node.append("svg:circle")
+    .attr("class", "node")
+    .attr("x", function(d) { return d.x; })
+    .attr("y", function(d) { return d.y; })
+    .attr("r", 10)
+    .style("stroke-width", 2)
+    .style("stroke", "white")
+    .attr("fill", function(d) { return color(POS_TO_NAME[d.cpostag].id); });
+    
+  node.append("svg:text")
+    .attr("font-size","16px")
+    .style("font-weight", "bold")
+    .attr("class", "nodetext")
+    .attr("dx", "15")
+    .attr("dy", "0")
+    .text(function(d) { return d.form });
 
-  node.append("title")
-      .text(function(d) { return d.id; });
+  node.append("svg:text")
+    .attr("font-size","12px")
+    .attr("class", "nodetext")
+    .attr("dx", 15)
+    .attr("dy", 12)
+    .text(function(d) { return d.cpostag });
 
   simulation
       .nodes(graph.nodes)
       .on("tick", ticked);
 
   simulation.force("link")
-      .links(graph.links);
+      .links(verticies);
 
   function ticked() {
     link
@@ -50,8 +114,7 @@ d3.json("miserables.json", function(error, graph) {
         .attr("y2", function(d) { return d.target.y; });
 
     node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
   }
 });
 
